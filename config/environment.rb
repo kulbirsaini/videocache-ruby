24a715603baf5b214479f8c8f982e189d3663681
Kulbir Saini <saini@saini.co.in>
2012-11-09 00:57:24 +0530

    app/views to be the views directory.

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,6 @@ Bundler.require if defined?(Bundler)
 APP_ROOT = Pathname.new(File.dirname(File.expand_path('../', __FILE__)))
 MIGRATIONS_DIR = APP_ROOT + 'db/migrate'
 DB_CONNECTION_INFO = YAML::load(File.open(APP_ROOT + 'config/database.yml'))
+VIEWS_DIR = APP_ROOT + 'app/views'
 
 Dir.glob(APP_ROOT + "app/models/*.rb").each{ |f| require f }
